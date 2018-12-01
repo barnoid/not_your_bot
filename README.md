643507943aaac780e6e9ef91b363f1abda5f41aa
Barney Livingston <barney@barnoid.org.uk>
2018-12-01 15:19:53 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Not Your Bot
+
+A bot that does the "Ladies, if your man" meme using ConceptNet. Output is piped to code that posts to Twitter or wherever.
+
+Word list needs to be all single words that can take _a_ or _an_ as an article (not _the_ or _some_).
+
+On twitter as https://twitter.com/notyourbot1
---
+++ notyour.rb
@@ -0,0 +1,120 @@
+#!/usr/bin/ruby
+
+require 'json'
+require 'open-uri'
+
+# Number of time to retry on failure
+TRIES = 4
+
+def pick_word
+	`shuf -n1 words`.chomp
+end
+
+def article(word)
+	if not word.match(/^(a |an |the )/i) then
+		if word.match(/^[aeiou]/i) then
+			word = "an #{word}"
+		else
+			word = "a #{word}"
+		end
+	end
+	return word
+end
+
+def get_cn_edges(word)
+	cn = JSON.parse(open("http://api.conceptnet.io/c/en/#{word}").read)
+	#puts JSON.pretty_generate(cn)
+	return cn['edges']
+end
+
+def parse_edges(edges, thing)
+	yourman = []
+
+	edges.each do |edge|
+
+		#puts "#{edge['rel']['@id']} #{edge['start']['@id']} #{edge['end']['@id']} - #{edge['start']['label']} #{edge['rel']['label']} #{edge['end']['label']}"
+
+		if edge['start']['@id'].match(/\/c\/en\/#{thing}(\/n)?/) then
+			# relationship of our thing to something else
+			case edge['rel']['@id']
+			when '/r/IsA'
+				yourman << "is #{article(edge['end']['label'])}"
+			when '/r/AtLocation'
+				yourman << "can be found in #{edge['end']['label']}"
+			when '/r/RelatedTo'
+				yourman << "is related to #{edge['end']['label']}"
+			when '/r/UsedFor'
+				yourman << "can be used for #{edge['end']['label']}"
+			when '/r/CapableOf'
+				yourman << "can #{edge['end']['label']}"
+			when '/r/HasContext'
+				yourman << "is related to #{edge['end']['label']}"
+			when '/r/Causes'
+				yourman << "causes #{edge['end']['label']}"
+			when '/r/DistinctFrom'
+				yourman << "is not #{edge['end']['label']}"
+			when '/r/HasProperty'
+				yourman << "is #{edge['end']['label']}"
+			when '/r/PartOf'
+				yourman << "is part of #{edge['end']['label']}"
+			when '/r/HasA'
+				yourman << "has #{edge['end']['label']}"
+			end
+		elsif edge['end']['@id'].match(/\/c\/en\/#{thing}(\/n)?/) then
+			# relationship of something else to our thing
+			case edge['rel']['@id']
+			when '/r/IsA'
+				yourman << "is #{article(edge['start']['label'])}"
+			when '/r/AtLocation'
+				yourman << "can contain #{edge['start']['label']}"
+			when '/r/RelatedTo'
+				yourman << "is related to #{edge['start']['label']}"
+			when '/r/UsedFor'
+				yourman << "can be used for #{edge['start']['label']}"
+			when '/r/PartOf'
+				yourman << "has #{article(edge['start']['label'])}"
+			when '/r/HasPrerequisite'
+				yourman << "is required for #{edge['start']['label']}"
+			end
+		end
+
+	end
+	return yourman
+end
+
+lines = []
+try = 0
+while try < TRIES
+	begin
+		thing = pick_word
+		edges = get_cn_edges(thing)
+		lines = parse_edges(edges, thing)
+	rescue => e
+		# probably ConceptNet lookup failed
+		STDERR.puts "#{e.inspect} try=#{try} thing=#{thing.inspect}"
+		try += 1
+		# exponential backoff
+		sleep (try * try) if try < TRIES
+		next
+	end
+	if lines.empty? then
+		# probably ConceptNet has nothing for us, try another word
+		STDERR.puts "lines empty try=#{try} thing=#{thing.inspect}"
+		try += 1
+		sleep 1
+		next
+	else
+		break
+	end
+end
+
+if lines.empty? then
+	STDERR.puts "lines empty tries exhausted"
+	exit 1
+end
+
+out  = "Ladies, if your man:\n\n"
+out += lines.uniq.sample(5).map{ |s| "- #{s.downcase}" }.join("\n")
+out += "\n\nHe's not your man. He's #{article(thing)}."
+
+puts out
